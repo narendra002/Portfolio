5ac9e3d0ee8fbbb3180f52105c25ba0a07f7ae0b
Narendra Choudhary <narendrac024@gmail.com>
2023-09-06 04:19:15 +0000

    Making Responsive

--- portfolio/src/Component/HeroSection.jsx
+++ portfolio/src/Component/HeroSection.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import Freelancer from '../assets/freelancer.svg'; // Adjust the path as needed
 import { Github, Linkedin } from '../assets/logo'; // Assuming Github and Linkedin are React components
- // Adjust the
+
 function HeroSection() {
   return (
-    <section className="bg-gray-100 py-14">
-      <div className="container mx-auto flex flex-col-reverse sm:flex-row items-center justify-between">
-        <div className="sm:w-1/2">
+    <section className="bg-gray-100 py-5 sm:py-5">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-center sm:justify-between">
+        <div className="sm:w-1/2 pr-8 text-center sm:text-left"> {/* Center text in small screens */}
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
             Narendra Choudhary
           </h1>
-          
-          <p className="text-base sm:text-lg md:text-xl font-light mb-8 color-#1f2937">
+
+          <p className="text-base sm:text-lg md:text-xl font-bold text-gray-600 mb-8"> {/* Change font to bold and color to gray */}
             A dedicated professional driven to craft end-to-end solutions that empower sustainable and scalable social and technical systems, with a relentless focus on creating meaningful impact.
           </p>
-          
-          <div className="flex items-center">
+
+          <div className="flex items-center justify-center sm:justify-start"> {/* Center icons in small screens */}
             <a
               href="https://github.com/narendra002/"
               target="_blank"
               rel="noopener noreferrer"
-              className="mr-4"
+              className="mr-4 text-blue-500 hover:underline"
             >
-              <Github className="w-1 h-1" /> {/* Adjust the size as needed */}
+              <Github className="w-4 h-4" /> {/* Adjust the size as needed */}
             </a>
-            
+
             <a
               href="https://www.linkedin.com/in/narendra-choudhary-7557301b6/"
               target="_blank"
               rel="noopener noreferrer"
-              className="mr-2"
+              className="text-blue-500 hover:underline"
             >
-              <Linkedin className="w-2 h-2" /> {/* Adjust the size as needed */}
+              <Linkedin className="w-4 h-4" /> {/* Adjust the size as needed */}
             </a>
           </div>
         </div>
-        
-        <div className="sm:w-1/2 mt-8 sm:mt-0"> {/* On small screens, move the image below */}
+
+        <div className="sm:w-1/2 mt-8 sm:mt-0"> {/* Center image in small screens */}
           <img
             src={Freelancer}
             alt="Freelancer Illustration"
@@ -44,7 +44,6 @@ function HeroSection() {
           />
         </div>
       </div>
-      
     </section>
   );
 }
--- portfolio/src/Component/Skills.jsx
+++ portfolio/src/Component/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Designer from '../assets/Designer.svg';
 import Hosting from '../assets/Hosting.svg';
 import {
@@ -12,48 +12,19 @@ import {
 } from '../assets/logo';
 
 function Skills() {
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('skills-animate'); // Add the animation class when the section is in view
-          }
-        });
-      },
-      {
-        threshold: 0.5, // You can adjust this threshold as needed
-      }
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    // Clean up the observer when the component unmounts
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-    };
-  }, []);
-
   return (
-    <section className="bg-gray-100 py-16" ref={sectionRef}>
+    <section className="bg-gray-100 py-5">
       <h1 className="font-bold text-center text-3xl sm:text-4xl md:text-5xl">
         What I Do?
       </h1>
-      <div className="container mx-auto flex flex-col-reverse sm:flex-row items-center justify-center">
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center justify-center">
         {/* Left Side (Skills Details) */}
-        <div className="sm:w-1/2 p-4">
+        <div className="lg:w-1/2 p-4">
           <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
             Full Stack Web Developer
           </h2>
-          <div className="flex items-center justify-between space-x-4">
-          <Html title="HTML" />
-
+          <div className="flex flex-wrap items-center space-x-4 lg:space-x-8 lg:justify-space justify-evenly">
+            <Html title="HTML" />
             <Css title="CSS" />
             <Javascript title="JavaScript" />
             <Reactjs title="React" />
@@ -66,51 +37,55 @@ function Skills() {
               src="https://img.icons8.com/nolan/64/mongo-db.png"
               alt="mongo-db"
             />
-            <img width="65" height="65" src="https://img.icons8.com/ios-filled/50/c-plus-plus-logo.png" alt="c-plus-plus-logo"/>
+            <img
+              width="65"
+              height="65"
+              src="https://img.icons8.com/ios-filled/50/c-plus-plus-logo.png"
+              alt="c-plus-plus-logo"
+            />
           </div>
 
           <ul className="list-disc list-inside mt-4">
-  <li className="mb-2">
-    ⚡ Building responsive websites and web applications using the MERN stack (MongoDB, Express.js, React, Node.js)
-  </li>
-  <li className="mb-2">
-    ⚡ Developing RESTful APIs and integrating them with front-end applications
-  </li>
-  <li className="mb-2">
-    ⚡ Designing and implementing database schemas using MongoDB
-  </li>
-  <li className="mb-2">
-    ⚡ Implementing user authentication and authorization
-  </li>
-  {/* Add more skills as needed */}
-</ul>
-
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
+              ⚡ Building responsive websites and web applications using the MERN stack (MongoDB, Express.js, React, Node.js)
+            </li>
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
+              ⚡ Developing RESTful APIs and integrating them with front-end applications
+            </li>
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
+              ⚡ Designing and implementing database schemas using MongoDB
+            </li>
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
+              ⚡ Implementing user authentication and authorization
+            </li>
+            {/* Add more skills as needed */}
+          </ul>
         </div>
 
         {/* Right Side (Image) */}
-        <div className="sm:w-1/2 p-4">
+        <div className="lg:w-1/2 p-4">
           <img
             src={Designer}
             alt="Designer Illustration"
-            className="mx-auto mix-blend-multiply image-animate"
+            className="mx-auto max-w-full h-auto mix-blend-multiply image-animate"
           />
         </div>
       </div>
 
       {/* Additional Section */}
-      <div className="container mx-auto flex flex-col-reverse sm:flex-row items-center justify-center mt-12">
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center justify-center mt-5">
         {/* Left Side (Skills Details) */}
-        <div className="sm:w-1/2 p-4">
+        <div className="lg:w-1/2 p-4">
           <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
             Cloud Infra-Architecture
           </h2>
-          <div className="flex items-center space-x-4 ">
+          <div className="flex flex-wrap items-center justify-center space-x-2 lg:space-x-4">
             <img
               width="65"
               height="65"
               src="https://img.icons8.com/color/48/firebase.png"
               alt="firebase"
-              className='mb-2'
+              className="mb-2"
             />
             <img
               width="65"
@@ -127,24 +102,25 @@ function Skills() {
           </div>
 
           <ul className="list-disc list-inside mt-4">
-            <li className='mb-2'>
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
               ⚡ Deploying web applications effortlessly on Vercel for lightning-fast hosting.
             </li>
-            <li className='mb-2'>
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
               ⚡ Utilizing Firebase for real-time database and authentication to power dynamic web experiences.
             </li>
-            <li className='mb-2'>
+            <li className="mb-2 text-gray-500 font-bold text-light-blue-500">
               ⚡ Streamlining continuous deployment with Vercel and Firebase for efficient updates and scaling.
             </li>
           </ul>
+
         </div>
 
         {/* Right Side (Image) */}
-        <div className="sm:w-1/2 p-4">
+        <div className="lg:w-1/2 p-4 sm:w-1/2 mt-8 sm:mt-0">
           <img
             src={Hosting}
             alt="Hosting Illustration"
-            className="mx-auto mix-blend-multiply image-animate"
+            className="mx-auto max-w-full h-auto mix-blend-multiply image-animate"
           />
         </div>
       </div>
